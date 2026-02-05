e9a7fbe768a021f26e2b63a771cc1041d0f3e953
Jamalludeen <jamalghazniwal@gmail.com>
2026-02-05 21:46:31 +0430

    setup app index

--- app/main.py
+++ app/main.py
@@ -1,25 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from typing import List
-from . import crud, models, schemas, database
+from fastapi import FastAPI
+from . import models, database
+from .routers import tasks
 
-router = APIRouter(
-    prefix="/tasks",
-    tags=["tasks"]
-)
+# Create Database Tables
+models.Base.metadata.create_all(bind=database.engine)
 
-@router.post("/", response_model=schemas.Task)
-def create_task(
-    task: schemas.TaskCreate,
-    db: Session = Depends(database.get_db),
-):
-    return crud.create_user_task(db=db, task=task)
+app = FastAPI(title="TaskMaster API")
 
-@router.get("/", response_model=List[schemas.Task])
-def read_tasks(
-    skip: int = 0, 
-    limit: int = 100, 
-    db: Session = Depends(database.get_db)
-):
-    tasks = crud.get_tasks(db, skip=skip, limit=limit)
-    return tasks
+# app.include_router(auth.router)
+app.include_router(tasks.router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to TaskMaster"}
