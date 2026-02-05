6028953a86b091efb82528cbe12b5b85a7e5cb7b
Jamalludeen <jamalghazniwal@gmail.com>
2026-02-05 21:44:53 +0430

    add tasks routes

---
+++ app/routers/tasks.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from . import crud, models, schemas, database
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["tasks"]
+)
+
+@router.post("/", response_model=schemas.Task)
+def create_task(
+    task: schemas.TaskCreate,
+    db: Session = Depends(database.get_db),
+):
+    return crud.create_user_task(db=db, task=task)
+
+@router.get("/", response_model=List[schemas.Task])
+def read_tasks(
+    skip: int = 0, 
+    limit: int = 100, 
+    db: Session = Depends(database.get_db)
+):
+    tasks = crud.get_tasks(db, skip=skip, limit=limit)
+    return tasks
