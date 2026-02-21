034128be05cb68b7b03663dfb38e84b3133faeb6
Jamalludeen <jamalghazniwal@gmail.com>
2026-02-21 20:03:03 +0430

    feat: register users router in application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from . import database, models
-from .routers import tasks
+from .routers import tasks, users
 
 # Create Database Tables
 models.Base.metadata.create_all(bind=database.engine)
@@ -9,6 +9,7 @@ app = FastAPI(title="TaskMaster API")
 
 # app.include_router(auth.router)
 app.include_router(tasks.router)
+app.include_router(users.router)
 
 @app.get("/")
 def root():
