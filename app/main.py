baf7d828c3b0c5111c352b3e40dd459a3aefdc0d
Jamalludeen <jamalghazniwal@gmail.com>
2026-09-12 21:20:41 +0430

    security(headers): prevent content type sniffing

--- app/main.py
+++ app/main.py
@@ -68,6 +68,7 @@ async def add_observability_headers(request: Request, call_next):
     response.headers["X-Process-Time"] = f"{process_time:.6f}"
     response.headers["X-API-Version"] = app.version
     response.headers["X-Service-Name"] = SERVICE_NAME
+    response.headers["X-Content-Type-Options"] = "nosniff"
     return response
 
 
